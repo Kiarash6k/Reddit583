53b8d94a73b7a5c05aedd2814acadad790970cbb
Kiarash6k <kowsarikiarash@gmail.com>
2022-05-26 17:34:15 +0430

    update WelcomePage to navigate to 2 diffrent pages

--- reddit583/lib/Screens/WelcomeScreen.dart
+++ reddit583/lib/Screens/WelcomeScreen.dart
@@ -9,53 +9,74 @@ class WelcomeScreen extends StatelessWidget {
         child: Scaffold(
             body: Center(
                 child: Column(
-                  children: [
-                    Container(
-                        margin: EdgeInsets.only(top: 444),
-                        child: RaisedButton(
-                          elevation: 15.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Colors.red),
+      children: [
+        Container(
+            margin: EdgeInsets.only(top: 444),
+            child: RaisedButton(
+              elevation: 15.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.red),
+              ),
+              child: Text('Login'),
+              textColor: Colors.black,
+              color: Colors.red,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(
+                        appBar: AppBar(
+                          title: Text('Login'),
+                          leading: IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
                           ),
-                          child: Text('Login'),
-                          textColor: Colors.black,
-                          color: Colors.red,
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (context) {
-                                return Scaffold();
-                              },
-                            ),
-                            );
-                          },
-                          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 110),
-                        )),
-                    Container(
-                        margin: EdgeInsets.only(top: 22),
-                        child: RaisedButton(
-                          elevation: 15.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Colors.white),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 110),
+            )),
+        Container(
+            margin: EdgeInsets.only(top: 22),
+            child: RaisedButton(
+              elevation: 15.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.white),
+              ),
+              child: Text('Signup'),
+              textColor: Colors.red,
+              color: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(
+                        appBar: AppBar(
+                          title: Text('Signup'),
+                          leading: IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
                           ),
-                          child: Text('Signup'),
-                          textColor: Colors.red,
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (context) {
-                                return Scaffold();
-                              },
-                            ),
-                            );
-                          },
-                          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 110),
-                        )),
-                  ],
-                )
-            )
-        )
-    );
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 110),
+            )),
+      ],
+    ))));
   }
 }
