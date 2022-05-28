8abec6cbc2618843204619a27a49e41f3328634f
Kiarash6k <kowsarikiarash@gmail.com>
2022-05-28 09:41:21 +0430

    Functionality of bottom navigation bar

--- reddit583/lib/Screens/Home_page.dart
+++ reddit583/lib/Screens/Home_page.dart
@@ -1,12 +1,39 @@
 // ignore_for_file: prefer_const_constructors, file_names, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
+
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:reddit583/Screens/WelcomeScreen.dart';
+import 'package:reddit583/pages/Community.dart';
+import 'package:reddit583/pages/Plus.dart';
+import 'package:reddit583/pages/Setting.dart';
+import 'package:reddit583/pages/feed.dart';
+import 'package:reddit583/pages/Comment.dart';
+import 'LoginScreen.dart';
+import 'SignUpScreen.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  String _currentTab = 'Home';
 
+  final Map<String, Widget> _children = {
+    'Home': feed(),
+    'Community': Community(),
+    'Plus': Plus(),
+    'Comment' : Comment(),
+    'Settings': Setting(),
+  };
+
+  changePage(int index){
+    setState(() {
+      _currentTab = _children.keys.elementAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +62,7 @@ class MyHomePage extends StatelessWidget {
       ),
     ],
   ),
+      body: _children[_currentTab],
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Color.fromARGB(255, 85, 240, 28),
         items: <Widget>[
@@ -45,7 +73,7 @@ class MyHomePage extends StatelessWidget {
           Icon(Icons.notifications, size: 30),
         ],
         onTap: (index) {
-          //Handle button tap
+          changePage(index);
         },
       ),
     );
