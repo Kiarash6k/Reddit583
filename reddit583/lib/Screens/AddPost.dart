65c9b89d19cbface910774f11da0cec2c8e5bf7d
sina eslami <sinaesl1126814@gmail.com>
2022-05-31 15:01:45 +0430

    add two textFields to the AddPost Class

--- reddit583/lib/Screens/AddPost.dart
+++ reddit583/lib/Screens/AddPost.dart
@@ -28,6 +28,54 @@ class _AddPostState extends State<AddPost> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('reddit'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.red,
+      ),
+      key: scaffoldKey,
+      body: Column(
+        children: [
+          Container(
+              alignment: Alignment.center,
+              width: 450.0,
+              margin: EdgeInsets.only(top: 15.0),
+              child: TextField(
+                controller: titleController,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  errorText: titleError,
+                  hintText: 'post title',
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                  contentPadding: EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
+                ),
+              )
+          ),
+          Container(
+              alignment: Alignment.center,
+              width: 450.0,
+              margin: EdgeInsets.only(top: 15.0),
+              child: TextField(
+                controller: textController,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  errorText: textError,
+                  hintText: 'post text',
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 1.5)),
+                  contentPadding: EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
+                ),
+              )
+          ),
+        ],
+      ),
+    );
   }
 }
