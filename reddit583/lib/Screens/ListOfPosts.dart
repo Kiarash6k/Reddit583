6c75daee5318fb90e22ce0eee14bc6668a01bd48
sina eslami <sinaesl1126814@gmail.com>
2022-05-31 14:38:31 +0430

    add Post text, title and its community to ListOfPosts

--- reddit583/lib/Screens/ListOfPosts.dart
+++ reddit583/lib/Screens/ListOfPosts.dart
@@ -16,6 +16,81 @@ class ShowPosts extends StatefulWidget {
 class _ShowPostsState extends State<ShowPosts> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SingleChildScrollView(
+      child: Card(
+        child: Column(
+          children: [
+            Material(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: NetworkImage(
+                      widget.community.getNetworkURL()),
+                ),
+                title: Text(widget.community.getCommunityName()),
+                subtitle: Text('u/user_name'),
+              ),),
+            Material(
+              child: ListTile(
+                title: Text(widget.post.getTitle()),
+                subtitle: Text(widget.post.getText()),
+              ),),
+            // Padding(
+            //   padding: EdgeInsets.symmetric(vertical: 16.0),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       Row(
+            //         children: [
+            //           GestureDetector(
+            //             child: widget.post.isPostLiked() ? Icon(
+            //                 Icons.thumb_up_alt) : Icon(
+            //                 Icons.thumb_up_alt_outlined),
+            //             onTap: () {
+            //               setState(() {
+            //                 widget.post.setLiked(!widget.post.isPostLiked());
+            //               });
+            //             },
+            //           ),
+            //           Padding(
+            //             padding: EdgeInsets.symmetric(horizontal: 12.0),
+            //             child: Text(widget.post.getVotes().toString(),
+            //                 style: TextStyle(fontSize: 16.0, color: Colors
+            //                     .black)),
+            //           ),
+            //           GestureDetector(
+            //               child: widget.post.isPostDislike() ? Icon(
+            //                   Icons.thumb_down_alt) : Icon(
+            //                   Icons.thumb_down_alt_outlined),
+            //               onTap: () {
+            //                 setState(() {
+            //                   widget.post.setDislike(!widget.post
+            //                       .isPostDislike());
+            //                 });
+            //               }
+            //           ),
+            //           Padding(
+            //             padding: EdgeInsets.symmetric(horizontal: 12.0),
+            //             child: Icon(Icons.chat_bubble_outlined),
+            //           )
+            //         ],
+            //       ),
+            //       GestureDetector(
+            //           child: widget.post.isPostMarked()
+            //               ? Icon(Icons.bookmark)
+            //               : Icon(Icons.bookmark_border_outlined),
+            //           onTap: () {
+            //             setState(() {
+            //               widget.post.setMarked(!widget.post.isPostMarked());
+            //             });
+            //           }
+            //       )
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
   }
 }
