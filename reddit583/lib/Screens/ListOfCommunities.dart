6b9c633aef5f8ea3af2c446da7e48123f0747b94
sina eslami <sinaesl1126814@gmail.com>
2022-05-31 13:42:56 +0430

    complete ListOfCommunities

--- reddit583/lib/Screens/ListOfCommunities.dart
+++ reddit583/lib/Screens/ListOfCommunities.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../MainClasses/Community.dart';
+
 
 class ShowCommunityList extends StatefulWidget {
-  const ShowCommunityList({Key key}) : super(key: key);
+  Community community;
+  Function removeCommunity;
+  ShowCommunityList(this.community, this.removeCommunity, {Key key})
+      : super(key: key);
 
   @override
   State<ShowCommunityList> createState() => _ShowCommunityListState();
@@ -11,6 +16,39 @@ class ShowCommunityList extends StatefulWidget {
 class _ShowCommunityListState extends State<ShowCommunityList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      shadowColor: Colors.teal,
+      elevation: 8.0,
+      child: ListTile(
+        title: Text(widget.community.getCommunityName(),
+            style: const TextStyle(
+                fontSize: 16.0, fontWeight: FontWeight.bold)),
+        leading: CircleAvatar(
+          radius: 20.0,
+          backgroundImage: NetworkImage(widget.community.getNetworkURL()),
+        ),
+        trailing: SizedBox(
+          width: 52.0,
+          child: Row(
+            children: [
+              GestureDetector(
+                child: const Icon(Icons.delete),
+                onTap: () {
+                  widget.removeCommunity();
+                },
+              ),
+              GestureDetector(
+                child: const Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return Scaffold();
+                  }));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
