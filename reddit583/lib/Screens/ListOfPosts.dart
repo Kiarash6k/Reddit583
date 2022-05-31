2308ab23a1951e175533e318c9bc85d8828728ba
sina eslami <sinaesl1126814@gmail.com>
2022-05-31 14:46:56 +0430

    add Post details (comment, like, dislike, save ...)

--- reddit583/lib/Screens/ListOfPosts.dart
+++ reddit583/lib/Screens/ListOfPosts.dart
@@ -35,59 +35,59 @@ class _ShowPostsState extends State<ShowPosts> {
                 title: Text(widget.post.getTitle()),
                 subtitle: Text(widget.post.getText()),
               ),),
-            // Padding(
-            //   padding: EdgeInsets.symmetric(vertical: 16.0),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //     children: [
-            //       Row(
-            //         children: [
-            //           GestureDetector(
-            //             child: widget.post.isPostLiked() ? Icon(
-            //                 Icons.thumb_up_alt) : Icon(
-            //                 Icons.thumb_up_alt_outlined),
-            //             onTap: () {
-            //               setState(() {
-            //                 widget.post.setLiked(!widget.post.isPostLiked());
-            //               });
-            //             },
-            //           ),
-            //           Padding(
-            //             padding: EdgeInsets.symmetric(horizontal: 12.0),
-            //             child: Text(widget.post.getVotes().toString(),
-            //                 style: TextStyle(fontSize: 16.0, color: Colors
-            //                     .black)),
-            //           ),
-            //           GestureDetector(
-            //               child: widget.post.isPostDislike() ? Icon(
-            //                   Icons.thumb_down_alt) : Icon(
-            //                   Icons.thumb_down_alt_outlined),
-            //               onTap: () {
-            //                 setState(() {
-            //                   widget.post.setDislike(!widget.post
-            //                       .isPostDislike());
-            //                 });
-            //               }
-            //           ),
-            //           Padding(
-            //             padding: EdgeInsets.symmetric(horizontal: 12.0),
-            //             child: Icon(Icons.chat_bubble_outlined),
-            //           )
-            //         ],
-            //       ),
-            //       GestureDetector(
-            //           child: widget.post.isPostMarked()
-            //               ? Icon(Icons.bookmark)
-            //               : Icon(Icons.bookmark_border_outlined),
-            //           onTap: () {
-            //             setState(() {
-            //               widget.post.setMarked(!widget.post.isPostMarked());
-            //             });
-            //           }
-            //       )
-            //     ],
-            //   ),
-            // )
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        child: widget.post.isPostLiked() ? Icon(
+                            Icons.thumb_up_alt) : Icon(
+                            Icons.thumb_up_alt_outlined),
+                        onTap: () {
+                          setState(() {
+                            widget.post.setLiked(!widget.post.isPostLiked());
+                          });
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Text(widget.post.getVotes().toString(),
+                            style: TextStyle(fontSize: 16.0, color: Colors
+                                .black)),
+                      ),
+                      GestureDetector(
+                          child: widget.post.isPostDislike() ? Icon(
+                              Icons.thumb_down_alt) : Icon(
+                              Icons.thumb_down_alt_outlined),
+                          onTap: () {
+                            setState(() {
+                              widget.post.setDislike(!widget.post
+                                  .isPostDislike());
+                            });
+                          }
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Icon(Icons.chat_bubble_outlined),
+                      )
+                    ],
+                  ),
+                  GestureDetector(
+                      child: widget.post.isPostMarked()
+                          ? Icon(Icons.bookmark)
+                          : Icon(Icons.bookmark_border_outlined),
+                      onTap: () {
+                        setState(() {
+                          widget.post.setMarked(!widget.post.isPostMarked());
+                        });
+                      }
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
